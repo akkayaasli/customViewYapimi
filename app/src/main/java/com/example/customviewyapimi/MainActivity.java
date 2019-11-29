4542239dacdcfba2014c37a91c8db15c0dbdec65
Aslı Akkaya <akkayaasli19@gmail.com>
2019-11-29 10:46:52 +0300

    Custom List View İle Rehberden Kişinin İsim ve Numarasının Çekildiği Mobil Uygulama Final

--- app/src/main/java/com/example/customviewyapimi/MainActivity.java
+++ app/src/main/java/com/example/customviewyapimi/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.customviewyapimi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ListView goster;
+    private CustomAdapter customAdapter;
+    private ArrayList<rehber> rehber2;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        goster=findViewById(R.id.listview);
+        /*goster=findViewById(R.id.listview);
 
         rehber[] kisiler=new rehber[]
                 {
@@ -20,6 +28,24 @@ public class MainActivity extends AppCompatActivity {
                         new rehber("Mihrimah Mercan","0558 188 1453",R.drawable.ic_launcher_background)
                 };
         rehberAdapter adapter=new rehberAdapter(this,R.layout.customlist,kisiler);
-        goster.setAdapter(adapter);
+        goster.setAdapter(adapter);*/
+        goster=findViewById(R.id.listview);
+        rehber2=new ArrayList<>();
+        Cursor phones=getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+" ASC");
+        while (phones.moveToNext()){
+            String name=phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String number=phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            //int resim= 0;
+            rehber rehber=new rehber();
+            rehber.setAd(name);
+            rehber.setNumara(number);
+            //rehber.setKisiResim(resim);
+            rehber2.add(rehber);
+            Log.d("name>>",name+" "+number);
+        }
+        phones.close();
+        customAdapter=new CustomAdapter(this,rehber2);
+        goster.setAdapter(customAdapter);
+
     }
 }
--- app/src/main/java/com/example/customviewyapimi/rehber.java
+++ app/src/main/java/com/example/customviewyapimi/rehber.java
@@ -6,33 +6,43 @@ public class rehber
     String numara;
     int kisiResim;
 
-    public rehber(String ad, String numara, int kisiResim) {
+    //public  rehber(){}
+
+    /*public rehber(String ad, String numara, int kisiResim)
+    {
+      //  super();//YENİ-1
         this.ad = ad;
         this.numara = numara;
         this.kisiResim = kisiResim;
-    }
+    }*/
 
-    public String getAd() {
+    public String getAd()
+    {
         return ad;
     }
 
-    public void setAd(String ad) {
+    public void setAd(String ad)
+    {
         this.ad = ad;
     }
 
-    public String getNumara() {
+    public String getNumara()
+    {
         return numara;
     }
 
-    public void setNumara(String numara) {
+    public void setNumara(String numara)
+    {
         this.numara = numara;
     }
 
-    public int getKisiResim() {
+    public int getKisiResim()
+    {
         return kisiResim;
     }
 
-    public void setKisiResim(int kisiResim) {
+    public void setKisiResim(int kisiResim)
+    {
         this.kisiResim = kisiResim;
     }
 }
